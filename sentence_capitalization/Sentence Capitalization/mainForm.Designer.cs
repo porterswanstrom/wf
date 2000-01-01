49649c35dfe16f7c31520b5e12674b9af7500c4d
​ <>
2000-01-01 00:00:00 +0000

    sentence capitalization

---
+++ sentence_capitalization/Sentence Capitalization/mainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Sentence_Capitalization
+{
+    partial class sentenceCapitalization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTxtBox = new System.Windows.Forms.TextBox();
+            this.outputLbl = new System.Windows.Forms.Label();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.helpLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTxtBox
+            // 
+            this.inputTxtBox.Location = new System.Drawing.Point(16, 71);
+            this.inputTxtBox.Name = "inputTxtBox";
+            this.inputTxtBox.Size = new System.Drawing.Size(340, 20);
+            this.inputTxtBox.TabIndex = 0;
+            this.inputTxtBox.Text = "this is an example sentence.";
+            // 
+            // outputLbl
+            // 
+            this.outputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLbl.Location = new System.Drawing.Point(16, 103);
+            this.outputLbl.Name = "outputLbl";
+            this.outputLbl.Size = new System.Drawing.Size(340, 23);
+            this.outputLbl.TabIndex = 1;
+            this.outputLbl.Text = "?";
+            this.outputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(150, 140);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(75, 36);
+            this.calculateBtn.TabIndex = 2;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(69, 145);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 27);
+            this.clearBtn.TabIndex = 3;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(231, 145);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 27);
+            this.exitBtn.TabIndex = 4;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // helpLbl
+            // 
+            this.helpLbl.AutoSize = true;
+            this.helpLbl.Location = new System.Drawing.Point(26, 44);
+            this.helpLbl.Name = "helpLbl";
+            this.helpLbl.Size = new System.Drawing.Size(323, 13);
+            this.helpLbl.TabIndex = 5;
+            this.helpLbl.Text = "Click calculate to make the first letter in every sentence uppercase.";
+            // 
+            // sentenceCapitalization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.helpLbl);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.outputLbl);
+            this.Controls.Add(this.inputTxtBox);
+            this.Name = "sentenceCapitalization";
+            this.Text = "Sentence Capitalization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTxtBox;
+        private System.Windows.Forms.Label outputLbl;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label helpLbl;
+    }
+}
+
---
+++ sentence_capitalization/Sentence Capitalization/mainForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sentence_Capitalization
+{
+    public partial class sentenceCapitalization : Form
+    {
+        public sentenceCapitalization()
+        {
+            InitializeComponent();
+        }
+
+        private string Capitalized(string str)
+        {
+            char[] ch = str.ToCharArray();
+            bool makeUpper = true;
+
+            for (int i = 0; i < ch.Length; i++)
+                if (makeUpper && char.IsLetter(ch[i])) {
+                    ch[i] = char.ToUpper(ch[i]);
+                    makeUpper = false;
+                }
+                else if (ch[i] == '.' || ch[i] == '?' || ch[i] == '!')
+                    makeUpper = true;
+
+            return new string(ch);
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            outputLbl.Text = Capitalized(inputTxtBox.Text);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            inputTxtBox.Text = "";
+            outputLbl.Text = "?";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
