f0bcc0044cc2e2a99c2215eb9417987d268df282
​ <>
2000-01-01 00:00:00 +0000

    factorial calc

---
+++ factorial_calc/Factorial Calculator/mainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Factorial_Calculator
+{
+    partial class factorialCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helpLbl = new System.Windows.Forms.Label();
+            this.inputTxtBox = new System.Windows.Forms.TextBox();
+            this.outputLbl = new System.Windows.Forms.Label();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // helpLbl
+            // 
+            this.helpLbl.AutoSize = true;
+            this.helpLbl.Location = new System.Drawing.Point(18, 31);
+            this.helpLbl.Name = "helpLbl";
+            this.helpLbl.Size = new System.Drawing.Size(321, 16);
+            this.helpLbl.TabIndex = 0;
+            this.helpLbl.Text = "Enter a nonnegative whole number to see its factorial.";
+            // 
+            // inputTxtBox
+            // 
+            this.inputTxtBox.Location = new System.Drawing.Point(119, 55);
+            this.inputTxtBox.Name = "inputTxtBox";
+            this.inputTxtBox.Size = new System.Drawing.Size(132, 22);
+            this.inputTxtBox.TabIndex = 1;
+            // 
+            // outputLbl
+            // 
+            this.outputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLbl.Location = new System.Drawing.Point(85, 88);
+            this.outputLbl.Name = "outputLbl";
+            this.outputLbl.Size = new System.Drawing.Size(194, 31);
+            this.outputLbl.TabIndex = 2;
+            this.outputLbl.Text = "?";
+            this.outputLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(57, 135);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 30);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(227, 135);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 30);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(138, 130);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(83, 40);
+            this.calcBtn.TabIndex = 3;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click_1);
+            // 
+            // factorialCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 193);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.outputLbl);
+            this.Controls.Add(this.inputTxtBox);
+            this.Controls.Add(this.helpLbl);
+            this.Name = "factorialCalc";
+            this.Text = "Factorial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label helpLbl;
+        private System.Windows.Forms.TextBox inputTxtBox;
+        private System.Windows.Forms.Label outputLbl;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button calcBtn;
+    }
+}
+
---
+++ factorial_calc/Factorial Calculator/mainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial_Calculator
+{
+    public partial class factorialCalc : Form
+    {
+        public factorialCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void calcBtn_Click_1(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputTxtBox.Text, out int input) && input > -1) {
+                long factorial = 1;
+
+                for (int i = 1; i <= input; i++)
+                    factorial *= i;
+
+                outputLbl.Text = factorial.ToString();
+            }
+            else
+                MessageBox.Show("Enter a nonnegative whole number.");
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            inputTxtBox.Text = "";
+            outputLbl.Text = "?";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
