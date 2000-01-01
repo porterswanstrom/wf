4e5d1367679b07c5455f649afd581764a44ec353
​ <>
2000-01-01 00:00:00 +0000

    name formatter

---
+++ name_formatter/Name Formatter/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Name_Formatter
+{
+    partial class nameFormatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.secondButton = new System.Windows.Forms.Button();
+            this.thirdButton = new System.Windows.Forms.Button();
+            this.forthButton = new System.Windows.Forms.Button();
+            this.fifthButton = new System.Windows.Forms.Button();
+            this.sixthButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.middleNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(30, 9);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(304, 16);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Fill out the boxes, then click the 1 through 6 buttons.";
+            // 
+            // firstButton
+            // 
+            this.firstButton.Location = new System.Drawing.Point(70, 166);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(38, 28);
+            this.firstButton.TabIndex = 9;
+            this.firstButton.Text = "1";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // secondButton
+            // 
+            this.secondButton.Location = new System.Drawing.Point(114, 166);
+            this.secondButton.Name = "secondButton";
+            this.secondButton.Size = new System.Drawing.Size(34, 28);
+            this.secondButton.TabIndex = 10;
+            this.secondButton.Text = "2";
+            this.secondButton.UseVisualStyleBackColor = true;
+            this.secondButton.Click += new System.EventHandler(this.secondButton_Click);
+            // 
+            // thirdButton
+            // 
+            this.thirdButton.Location = new System.Drawing.Point(154, 166);
+            this.thirdButton.Name = "thirdButton";
+            this.thirdButton.Size = new System.Drawing.Size(37, 28);
+            this.thirdButton.TabIndex = 11;
+            this.thirdButton.Text = "3";
+            this.thirdButton.UseVisualStyleBackColor = true;
+            this.thirdButton.Click += new System.EventHandler(this.thirdButton_Click);
+            // 
+            // forthButton
+            // 
+            this.forthButton.Location = new System.Drawing.Point(197, 166);
+            this.forthButton.Name = "forthButton";
+            this.forthButton.Size = new System.Drawing.Size(38, 28);
+            this.forthButton.TabIndex = 12;
+            this.forthButton.Text = "4";
+            this.forthButton.UseVisualStyleBackColor = true;
+            this.forthButton.Click += new System.EventHandler(this.forthButton_Click);
+            // 
+            // fifthButton
+            // 
+            this.fifthButton.Location = new System.Drawing.Point(241, 166);
+            this.fifthButton.Name = "fifthButton";
+            this.fifthButton.Size = new System.Drawing.Size(34, 28);
+            this.fifthButton.TabIndex = 13;
+            this.fifthButton.Text = "5";
+            this.fifthButton.UseVisualStyleBackColor = true;
+            this.fifthButton.Click += new System.EventHandler(this.fifthButton_Click);
+            // 
+            // sixthButton
+            // 
+            this.sixthButton.Location = new System.Drawing.Point(281, 166);
+            this.sixthButton.Name = "sixthButton";
+            this.sixthButton.Size = new System.Drawing.Size(37, 28);
+            this.sixthButton.TabIndex = 14;
+            this.sixthButton.Text = "6";
+            this.sixthButton.UseVisualStyleBackColor = true;
+            this.sixthButton.Click += new System.EventHandler(this.sixthButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(45, 208);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(300, 35);
+            this.outputLabel.TabIndex = 15;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(196, 256);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 34);
+            this.exitButton.TabIndex = 17;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(106, 256);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 34);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(57, 48);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(151, 16);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Enter your preferred title:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(77, 129);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(131, 16);
+            this.lastNameLabel.TabIndex = 7;
+            this.lastNameLabel.Text = "Enter your last name:";
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.Location = new System.Drawing.Point(57, 101);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(151, 16);
+            this.middleNameLabel.TabIndex = 5;
+            this.middleNameLabel.Text = "Enter your middle name:";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(78, 76);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(130, 16);
+            this.firstNameLabel.TabIndex = 3;
+            this.firstNameLabel.Text = "Enter your first name:";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(223, 45);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(100, 22);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(223, 129);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.lastNameTextBox.TabIndex = 8;
+            // 
+            // middleNameTextBox
+            // 
+            this.middleNameTextBox.Location = new System.Drawing.Point(223, 101);
+            this.middleNameTextBox.Name = "middleNameTextBox";
+            this.middleNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.middleNameTextBox.TabIndex = 6;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(223, 73);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.firstNameTextBox.TabIndex = 4;
+            // 
+            // nameFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 303);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.middleNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.middleNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.sixthButton);
+            this.Controls.Add(this.fifthButton);
+            this.Controls.Add(this.forthButton);
+            this.Controls.Add(this.thirdButton);
+            this.Controls.Add(this.secondButton);
+            this.Controls.Add(this.firstButton);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "nameFormatter";
+            this.Text = "Name Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button secondButton;
+        private System.Windows.Forms.Button thirdButton;
+        private System.Windows.Forms.Button forthButton;
+        private System.Windows.Forms.Button fifthButton;
+        private System.Windows.Forms.Button sixthButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox middleNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+    }
+}
+
---
+++ name_formatter/Name Formatter/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Formatter
+{
+    public partial class nameFormatter : Form
+    {
+        public nameFormatter()
+        {
+            InitializeComponent();
+        }
+
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = titleTextBox.Text + " " +
+                firstNameTextBox.Text + " " +
+                middleNameTextBox.Text + " " +
+                lastNameTextBox.Text;
+        }
+
+        private void secondButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = firstNameTextBox.Text + " " +
+                middleNameTextBox.Text + " " +
+                lastNameTextBox.Text;
+        }
+
+        private void thirdButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = firstNameTextBox.Text + " " +
+                lastNameTextBox.Text;
+        }
+
+        private void forthButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = lastNameTextBox.Text + ", " +
+                firstNameTextBox.Text + " " +
+                middleNameTextBox.Text + ", " +
+                titleTextBox.Text;
+        }
+
+        private void fifthButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = lastNameTextBox.Text + ", " +
+                firstNameTextBox.Text + " " +
+                middleNameTextBox.Text;
+        }
+
+        private void sixthButton_Click(object sender, EventArgs e)
+        {
+            outputLabel.Text = lastNameTextBox.Text + ", " +
+                firstNameTextBox.Text;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Text = "";
+            firstNameTextBox.Text = "";
+            middleNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            outputLabel.Text = "";
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
