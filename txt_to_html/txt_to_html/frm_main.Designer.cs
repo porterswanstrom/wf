1d14a680aaee1e4da35042fa7defbea34c68f626
​ <>
2000-01-01 00:00:00 +0000

    txt to html

---
+++ txt_to_html/txt_to_html/frm_main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace txt_to_html
+{
+    partial class frm_main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_help = new System.Windows.Forms.Label();
+            btn_convert = new System.Windows.Forms.Button();
+            btn_exit = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // lbl_help
+            // 
+            lbl_help.AutoSize = true;
+            lbl_help.Location = new System.Drawing.Point(43, 49);
+            lbl_help.Name = "lbl_help";
+            lbl_help.Size = new System.Drawing.Size(204, 15);
+            lbl_help.TabIndex = 0;
+            lbl_help.Text = "Upload a .txt file to convert it to html.";
+            // 
+            // btn_convert
+            // 
+            btn_convert.ForeColor = System.Drawing.Color.Green;
+            btn_convert.Location = new System.Drawing.Point(78, 79);
+            btn_convert.Name = "btn_convert";
+            btn_convert.Size = new System.Drawing.Size(129, 32);
+            btn_convert.TabIndex = 1;
+            btn_convert.Text = "Upload and Convert";
+            btn_convert.UseVisualStyleBackColor = true;
+            btn_convert.Click += btn_convert_Click;
+            // 
+            // btn_exit
+            // 
+            btn_exit.ForeColor = System.Drawing.Color.Maroon;
+            btn_exit.Location = new System.Drawing.Point(101, 117);
+            btn_exit.Name = "btn_exit";
+            btn_exit.Size = new System.Drawing.Size(75, 32);
+            btn_exit.TabIndex = 2;
+            btn_exit.Text = "Exit";
+            btn_exit.UseVisualStyleBackColor = true;
+            btn_exit.Click += btn_exit_Click;
+            // 
+            // frm_main
+            // 
+            AcceptButton = btn_convert;
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = btn_exit;
+            ClientSize = new System.Drawing.Size(284, 201);
+            Controls.Add(btn_exit);
+            Controls.Add(btn_convert);
+            Controls.Add(lbl_help);
+            Name = "frm_main";
+            Text = "Text to html";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_help;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
---
+++ txt_to_html/txt_to_html/frm_main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* to read/write files */
+using System.IO;
+
+namespace txt_to_html
+{
+	public partial class frm_main : Form
+	{
+		public frm_main()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_convert_Click(object sender, EventArgs e)
+		{
+			/* show a dialog that lets the user choose a text file to read */
+			OpenFileDialog txt = new OpenFileDialog();
+			txt.Filter = "Text File|*.txt";
+			if (txt.ShowDialog() != DialogResult.OK)
+				return;
+
+			/* show a dialog that lets the user create an output html file */
+			SaveFileDialog html = new SaveFileDialog();
+			html.Filter = "Hyper Text Markup File|*.html";
+			if (html.ShowDialog() != DialogResult.OK)
+				return;
+
+			/* copy the contents of the selected text file to an array */
+			string[] input = File.ReadAllLines(txt.FileName);
+
+			/* create the output html file and start writing the html */
+			StreamWriter output = File.CreateText(html.FileName);
+			output.WriteLine("<html><body bgcolor='#F6F3F0'>");
+			for (int i = 0; i < input.Count(); i++)
+				/* check for special syntax and convert it to html */
+				switch (input[i]) {
+					case string s when s.StartsWith("	"):
+						output.WriteLine(input[i].Replace("	",
+							"<p>") + "</p>");
+						break;
+					case string s when s.StartsWith("! "):
+						output.WriteLine(input[i].Replace("! ",
+							"<a href='http://") + "'>" +
+							input[i].Replace("! ", "") + "</a>");
+						break;
+					case string s when s.StartsWith("# "):
+						output.WriteLine(input[i].Replace("# ",
+							"<h1>") + "</h1>");
+						break;
+					case string s when s.StartsWith("## "):
+						output.WriteLine(input[i].Replace("## ",
+							"<h2>") + "</h2>");
+						break;
+					case string s when s.StartsWith("### "):
+						output.WriteLine(input[i].Replace("### ",
+							"<h3>") + "</h3>");
+						break;
+				}
+			output.WriteLine("</body></html>");
+
+			/* close the file and tell the user that the conversion was successful */
+			output.Close();
+			MessageBox.Show("Converted Successfully! Your html file is located at: " + html.FileName);
+		}
+
+		private void btn_exit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
